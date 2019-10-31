70bfe3e7a95d9104a1a466f92ac1331395e1decf
Anastasia Kyrazoglou <arkyratzog@gmail.com>
2019-10-31 14:06:00 -0400

    9.2 UI tour + Creating the Spot and Spots classes

---
+++ Snacktacular/Spots.swift
@@ -0,0 +1,14 @@
+//
+//  Spots.swift
+//  Snacktacular
+//
+//  Created by Anastasia on 10/29/19.
+//  Copyright © 2019 John Gallaugher. All rights reserved.
+//
+
+import Foundation
+
+
+class Spots{
+    var spotArray = [Spot]()
+}
