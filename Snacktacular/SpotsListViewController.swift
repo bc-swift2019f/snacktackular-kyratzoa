4ef8004d206fb4e103228b7623c37ac4cf0f2f35
Anastasia Kyrazoglou <arkyratzog@gmail.com>
2019-11-03 10:53:13 -0500

    9.4 Adding Google Place Autocomplete

--- Snacktacular/SpotsListViewController.swift
+++ Snacktacular/SpotsListViewController.swift
@@ -23,7 +23,7 @@ class SpotsListViewController: UIViewController {
         super.viewDidLoad()
         
         authUI = FUIAuth.defaultAuthUI()
-        authUI?.delegate = self
+        authUI?.delegate = self as! FUIAuthDelegate
         
         tableView.dataSource = self
         tableView.delegate = self
