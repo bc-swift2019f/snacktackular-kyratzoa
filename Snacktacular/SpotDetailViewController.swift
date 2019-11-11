90cb8fdeef21bbc5101fe7431ac64131d08493f5
Anastasia Kyrazoglou <arkyratzog@gmail.com>
2019-11-10 22:42:33 -0500

    9.16 The Photo and Photos Classes, and Creating an Action Sheet Alert Learning Objective

--- Snacktacular/SpotDetailViewController.swift
+++ Snacktacular/SpotDetailViewController.swift
@@ -24,6 +24,7 @@ class SpotDetailViewController: UIViewController {
     
     var spot: Spot!
     var reviews: Reviews!
+    var photos: Photos!
     let regionDistance: CLLocationDistance = 750    // 750 meters or about a half a mile
     var locationManager: CLLocationManager!
     var currentLocation: CLLocation!
@@ -37,6 +38,9 @@ class SpotDetailViewController: UIViewController {
         
         tableView.dataSource = self
         tableView.delegate = self
+        
+        collectionView.dataSource = self
+        collectionView.delegate = self
        
         //mapView.delegate = self
         
@@ -60,6 +64,7 @@ class SpotDetailViewController: UIViewController {
         }
         
         reviews = Reviews()
+        photos = Photos()
         
         let region = MKCoordinateRegion (center: spot.coordinate, latitudinalMeters: regionDistance, longitudinalMeters: regionDistance)
         mapView.setRegion(region, animated: true)
@@ -122,7 +127,17 @@ class SpotDetailViewController: UIViewController {
             navigationController?.popViewController(animated: true)
         }
     }
-
+  
+    func cameraOrLibraryAlert(){
+        let alertController = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
+        let cameraAction = UIAlertAction(title: "Camera", style: .default, handler: nil)
+        let photoLibraryAction = UIAlertAction(title: "Photo Library", style: .default, handler: nil)
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        alertController.addAction(cameraAction)
+        alertController.addAction(photoLibraryAction)
+        alertController.addAction(cancelAction)
+        present(alertController, animated: true, completion: nil)
+    }
 
     @IBAction func textFieldEditingChanged(_ sender: UITextField) {
         saveBarButton.isEnabled = !(nameField.text == "")
@@ -138,6 +153,7 @@ class SpotDetailViewController: UIViewController {
     
     
     @IBAction func photoButtonPressed(_ sender: UIButton) {
+        cameraOrLibraryAlert()
     }
     
     @IBAction func reviewButtonPressed(_ sender: UIButton) {
@@ -282,5 +298,18 @@ extension SpotDetailViewController: UITableViewDelegate, UITableViewDataSource{
         cell.review = reviews.reviewArray[indexPath.row]
         return cell
     }
+}
+
+extension SpotDetailViewController: UICollectionViewDataSource, UICollectionViewDelegate{
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return photos.photoArray.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "PhotoCell", for: indexPath) as! SpotPhotosCollectionViewCell
+        cell.photo = photos.photoArray[indexPath.row]
+        return cell
+    }
+    
     
 }
